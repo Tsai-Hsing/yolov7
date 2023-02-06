7646bf160c475f2c9b72c98c34741191188a41b6
Tsai-Hsing <38457390+Tsai-Hsing@users.noreply.github.com>
2023-02-06 16:57:53 +0800

    Add files via upload

---
+++ test_pre.py
@@ -0,0 +1,23 @@
+from funcOD import *
+from PIL import Image
+import sys
+
+try:
+
+    resultData = mainPreLoadModel('./testfolder/')
+    #mainPredict('','./testfolder/', '' ,xx['result'], '', '')
+    im = Image.open('./testfolder/test1_1.png')
+    mainPredict(im, './testfolder/' , '', resultData['result'], '', '')
+    
+    #mainPredict(im, './testfoler/' , '', resultData['result'], '', '')
+    #mainPredict(im, './testfoler/' , '', resultData['result'], '', '')
+    #mainPredict(im, './testfoler/' , '', resultData['result'], '', '')
+    #mainPredict(im, './testfoler/' , '', resultData['result'], '', '')
+    #mainPredict(im, './testfoler/' , '', resultData['result'], '', '')
+
+    #res = test_detector.test(im, './testfolder/','','')
+    #print(res)
+
+except:
+    print("Unexpected error:", sys.exc_info()[0])
+
