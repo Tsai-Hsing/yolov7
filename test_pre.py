1ffd4a7c48689beb8e3d29c70c01b8abdd275156
Tsai-Hsing <38457390+Tsai-Hsing@users.noreply.github.com>
2023-03-14 13:34:41 +0800

    Update test_pre.py

--- test_pre.py
+++ test_pre.py
@@ -7,7 +7,7 @@ try:
     resultData = mainPreLoadModel('./testfolder/')
     #mainPredict('','./testfolder/', '' ,xx['result'], '', '')
     im = Image.open('./testfolder/test1_1.png')
-    return mainPredict(im, '/content/yolov7/testfolder/' , '', resultData['result'], '', '')
+    mainPredict(im, '/content/yolov7/testfolder/' , '', resultData['result'], '', '')
     
     #mainPredict(im, './testfoler/' , '', resultData['result'], '', '')
     #mainPredict(im, './testfoler/' , '', resultData['result'], '', '')
